13d90968e76ec90a261d58a7c243f38ca5f523de
fran <frcama@xuqueralaumnat.es>
2024-05-15 11:22:07 +0200

    Scenne Builder

---
+++ src/main/java/com/example/proyecto/Inicio_Sesion.java
@@ -0,0 +1,41 @@
+package com.example.proyecto;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class Inicio_Sesion
+{
+    @javafx.fxml.FXML
+    private Text id_email;
+    @javafx.fxml.FXML
+    private TextField pass;
+    @javafx.fxml.FXML
+    private ImageView tiktok;
+    @javafx.fxml.FXML
+    private Text id_pass;
+    @javafx.fxml.FXML
+    private Button crear_cuenta;
+    @javafx.fxml.FXML
+    private ImageView logo;
+    @javafx.fxml.FXML
+    private Button inicio;
+    @javafx.fxml.FXML
+    private ImageView instagram;
+    @javafx.fxml.FXML
+    private TextField email;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void inicio(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void crear(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/proyecto/Registro.java
@@ -0,0 +1,23 @@
+package com.example.proyecto;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+
+public class Registro {
+    @javafx.fxml.FXML
+    private CheckBox Aceptar;
+    @javafx.fxml.FXML
+    private Text Condiciones;
+    @javafx.fxml.FXML
+    private Button Crear_Cuenta;
+
+    @javafx.fxml.FXML
+    public void Aceptar(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Crear_Cuenta(ActionEvent actionEvent) {
+    }
+}
