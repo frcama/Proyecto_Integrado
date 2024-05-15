aa1e76d6d2aebd62cc287f9a2cc4052279ce7192
frcama <150140609+frcama@users.noreply.github.com>
2024-05-15 13:11:52 +0200

    Add files via upload

---
+++ Proyecto/src/main/java/com/example/proyecto/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.example.proyecto;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("inicio_sesion.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 900);
+        stage.setTitle("EstuBook");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
