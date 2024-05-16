cf58ff4068a70072cde9a1f47513bbf02e15f4a6
arnandisQuique <arnansiquique@gmail.com>
2024-05-16 14:30:25 +0530

    Creación mostrarLibros en LibrosModel

---
+++ Proyecto/src/main/java/com/example/proyecto/LibrosModel.java
@@ -0,0 +1,37 @@
+package com.example.proyecto;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class LibrosModel extends Conexion{
+
+    public ArrayList<Libros> mostrarLibros(){
+
+        ArrayList<Libros> listaLibros = new ArrayList<>();
+
+        try {
+            String sql = "Select * from libros;";
+
+            PreparedStatement ps = this.getConexion().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+
+            while (rs.next()){
+                listaLibros.add(new Libros(rs.getString(1),rs.getString(2),rs.getString(3), rs.getDouble(4),rs.getInt(5)));
+
+                return listaLibros;
+            }
+        } catch (SQLException e) {
+            e.getMessage().toString();
+        }
+        return listaLibros;
+
+    }
+
+
+}
