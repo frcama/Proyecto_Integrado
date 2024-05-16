2c67f9de0e626b50b19b38ff932dedfb0f3bcc3e
arnandisQuique <arnansiquique@gmail.com>
2024-05-16 15:54:56 +0530

    Cambios

--- Proyecto/src/main/java/com/example/proyecto/Usuario.java
+++ Proyecto/src/main/java/com/example/proyecto/Usuario.java
@@ -5,11 +5,16 @@ public class Usuario {
     private String nombre;
     private String apellido;
     private String correo;
+    private String DNI;
+    private int edad;
 
-    public Usuario(String nombre, String apellido, String correo) {
+
+    public Usuario(String nombre, String apellido, String correo, String DNI, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
+        this.DNI = DNI;
+        this.edad = edad;
     }
 
     public String getNombre() {
@@ -34,5 +39,21 @@ public class Usuario {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 }
