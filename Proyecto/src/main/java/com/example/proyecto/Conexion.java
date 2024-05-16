0827230ecf8b7d58ae1d90e005f54a07cf1cafa2
arnandisQuique <arnansiquique@gmail.com>
2024-05-16 14:21:16 +0530

    Conexion creada

--- Proyecto/src/main/java/com/example/proyecto/Conexion.java
+++ Proyecto/src/main/java/com/example/proyecto/Conexion.java
@@ -8,20 +8,30 @@ import java.sql.SQLException;
 public class Conexion {
     Connection conexion = null;
 
-    public Connection getConexion() {
-        String cadenaConexion = "jdbc:mysql://localhost:3306/usuarios_db";
-        String usuario = "root";
-        String password = "1234";
+    private java.sql.Connection conn;
+    private String cadenaConexion = "jdbc:mysql://localhost:3306/entregableProgramació";
+    private String nombreUsuario = "root";
+    private String password = "1234";
+
+    public java.sql.Connection getConexion() {
 
         try {
-
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            this.conexion = (Connection) DriverManager.getConnection(cadenaConexion, usuario, password);
+            this.conn = DriverManager.getConnection(this.cadenaConexion, this.nombreUsuario, this.password);
+            return this.conn;
 
         } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
+    }
 
-        return conexion;
+
+    public void cerrarConexion() {
+        try {
+            this.conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
