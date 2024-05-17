366607501dd90f2cf156f3fb696a90f9d4c11320
alex <palomares.academia@gmail.com>
2024-05-17 11:55:08 +0200

    cambios

--- Proyecto/src/main/java/com/example/proyecto/Novedades.java
+++ Proyecto/src/main/java/com/example/proyecto/Novedades.java
@@ -1,4 +1,22 @@
 package com.example.proyecto;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 public class Novedades {
+    @javafx.fxml.FXML
+    private Pane panelDesplegableNOVEDADES;
+    @javafx.fxml.FXML
+    private AnchorPane PanelNovedades;
+    @javafx.fxml.FXML
+    private Button novedadesBOTON;
+    @javafx.fxml.FXML
+    private Button PerfilBOTON;
+    @javafx.fxml.FXML
+    private Button alquileresBOTON;
+    @javafx.fxml.FXML
+    private Button eventosBOTON;
+    @javafx.fxml.FXML
+    private Button librosBOTON;
 }
