53f9940074186d8e6a878e84aaf47f2ea5dc0f72
arnandisQuique <arnansiquique@gmail.com>
2024-05-21 14:12:14 +0530

    ImprimirImagen

--- Proyecto/src/main/java/com/example/proyecto/Anyadir.java
+++ Proyecto/src/main/java/com/example/proyecto/Anyadir.java
@@ -1,15 +1,27 @@
 package com.example.proyecto;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Anyadir
 {
+
+    FileChooser fileChooser = new FileChooser();
+
     @javafx.fxml.FXML
     private AnchorPane PanelAnyadir;
     @javafx.fxml.FXML
@@ -21,9 +33,11 @@ public class Anyadir
     @javafx.fxml.FXML
     private Button subirButton;
     @javafx.fxml.FXML
-    private ImageView muestraImagenChoiceBox;
+    private ImageView muestraImagen;
     @javafx.fxml.FXML
     private ChoiceBox categoriaChoiceBox;
+    @javafx.fxml.FXML
+    private Button atrasButton;
 
 
     @javafx.fxml.FXML
@@ -32,5 +46,21 @@ public class Anyadir
 
     @javafx.fxml.FXML
     public void OnSubirImagenbutton(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(new Stage());
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            muestraImagen.setImage(image);
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void onAtrasButtonClick(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("Novedades.fxml"));
+            this.PanelAnyadir.getChildren().setAll(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
