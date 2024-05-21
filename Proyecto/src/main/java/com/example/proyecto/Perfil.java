8238081718b19a37d2d72f53040c1062d27b3bf5
arnandisQuique <arnansiquique@gmail.com>
2024-05-21 15:41:13 +0530

    cambios en el perfil

--- Proyecto/src/main/java/com/example/proyecto/Perfil.java
+++ Proyecto/src/main/java/com/example/proyecto/Perfil.java
@@ -3,19 +3,41 @@ package com.example.proyecto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Perfil {
+    FileChooser fileChooser = new FileChooser();
+
     @javafx.fxml.FXML
     private Button atrasDePerfilBOTON;
     @javafx.fxml.FXML
     private AnchorPane panelDePerfil;
     @javafx.fxml.FXML
     private Button EditarFotoPerfil;
+    @javafx.fxml.FXML
+    private TextField nombreusuarioPerfil;
+    @javafx.fxml.FXML
+    private TextField descripccionPerfil;
+    @javafx.fxml.FXML
+    private TextField nombrePerfil;
+    @javafx.fxml.FXML
+    private DatePicker fechaNacimientoPerfil;
+    @javafx.fxml.FXML
+    private PasswordField contrsaenaPerfil;
+    @javafx.fxml.FXML
+    private ImageView fotoPerfil;
 
     @javafx.fxml.FXML
     public void atrasDePerfilBOTONclick(ActionEvent actionEvent) {
@@ -31,5 +53,12 @@ public class Perfil {
 
     @javafx.fxml.FXML
     public void EditarFotoPerfilclick(ActionEvent actionEvent) {
+
+        File file = fileChooser.showOpenDialog(new Stage());
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            fotoPerfil.setImage(image);
+        }
     }
 }
