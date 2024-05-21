7d8eba03d6144351ca7ad8013296ada92f11884c
arnandisQuique <arnansiquique@gmail.com>
2024-05-21 13:36:06 +0530

    Alquileres

--- Proyecto/src/main/java/com/example/proyecto/Alquileres.java
+++ Proyecto/src/main/java/com/example/proyecto/Alquileres.java
@@ -9,6 +9,55 @@ public class Alquileres {
     private double precio;
     private double metrosCuadrados;
 
+    public Alquileres(int id_Alquiler, String lugar, String nombre, double precio, double metrosCuadrados) {
+        this.id_Alquiler = id_Alquiler;
+        this.lugar = lugar;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.metrosCuadrados = metrosCuadrados;
+    }
 
+    public Alquileres(){
 
+    }
+
+    public int getId_Alquiler() {
+        return id_Alquiler;
+    }
+
+    public void setId_Alquiler(int id_Alquiler) {
+        this.id_Alquiler = id_Alquiler;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void setMetrosCuadrados(double metrosCuadrados) {
+        this.metrosCuadrados = metrosCuadrados;
+    }
 }
