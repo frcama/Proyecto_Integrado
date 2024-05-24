b5390ad54637d44245be0e8b3a4e65cf442380aa
fran <frcama@xuqueralaumnat.es>
2024-05-24 10:40:12 +0200

    Modo Oscuro

--- Proyecto/src/main/java/com/example/proyecto/LibrosOS.java
+++ Proyecto/src/main/java/com/example/proyecto/LibrosOS.java
@@ -1,4 +1,62 @@
 package com.example.proyecto;
 
 public class LibrosOS {
+    @javafx.fxml.FXML
+    private ColumnConstraints col2GripPane;
+    @javafx.fxml.FXML
+    private ChoiceBox precioFiltroChoicebox;
+    @javafx.fxml.FXML
+    private ChoiceBox nHabitacioneschoiceBox;
+    @javafx.fxml.FXML
+    private ColumnConstraints col1GripPane;
+    @javafx.fxml.FXML
+    private ImageView PerfilBOTON1;
+    @javafx.fxml.FXML
+    private Button eventosBOTON2;
+    @javafx.fxml.FXML
+    private GridPane cosasGripPane;
+    @javafx.fxml.FXML
+    private Button novedadesBOTON2;
+    @javafx.fxml.FXML
+    private Button perfilBOTON2;
+    @javafx.fxml.FXML
+    private ChoiceBox zonaFiltroChoicebox;
+    @javafx.fxml.FXML
+    private Button alquileresBOTON2;
+    @javafx.fxml.FXML
+    private Button librosBOTON2;
+    @javafx.fxml.FXML
+    private Button filtrarButton2;
+    @javafx.fxml.FXML
+    private AnchorPane PanelDeLibros;
+    @javafx.fxml.FXML
+    private Button addButton2;
+
+    @javafx.fxml.FXML
+    public void perfilBOTON2click(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void eventosBOTON2click(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void librosBOTON2click(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onAdd2Clicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void alquileresBOTON2click(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void novedadesBOTON2click(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void filtrarBotton2Click(ActionEvent actionEvent) {
+    }
 }
