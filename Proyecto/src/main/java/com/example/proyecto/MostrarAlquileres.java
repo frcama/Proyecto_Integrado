562f37c389b21c40635985a1f3852dc32bd2fbf3
fran <frcama@xuqueralaumnat.es>
2024-05-30 13:28:27 +0200

    Comentado las clases de Mostrar

--- Proyecto/src/main/java/com/example/proyecto/MostrarAlquileres.java
+++ Proyecto/src/main/java/com/example/proyecto/MostrarAlquileres.java
@@ -4,8 +4,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-
+// Clase MostrarAlquileres extiende de Conexion, permite mostrar la información de un alquiler específico
 public class MostrarAlquileres extends Conexion{
+    // Definición de los componentes de la interfaz gráfica
     @javafx.fxml.FXML
     private Label nombreAlquilerLabel;
     @javafx.fxml.FXML
@@ -22,18 +23,20 @@ public class MostrarAlquileres extends Conexion{
     private AnchorPane panelMostrarAlquiler;
     @javafx.fxml.FXML
     private Label descripcionAlquilerLabel;
-
+    // Instancia de Alquileres
     Alquileres alquiler = new Alquileres();
     @javafx.fxml.FXML
     private Label ciudadAlquiler;
-
+    // Método para establecer los datos del alquiler en los componentes de la interfaz gráfica
     public void setData(Alquileres alquiler) {
-
+        // Asignar el objeto Alquiler recibido a la instancia local
         this.alquiler = alquiler;
+        // Crear una instancia de PerfilModel y Usuario
         PerfilModel pm= new PerfilModel();
         Usuario u = new Usuario();
+        // Obtener el perfil del usuario
         u = pm.perfilModel(u.getCorreo(),u.getContra());
-
+        // Establecer los datos del alquiler en los labels correspondientes
         nombreAlquilerLabel.setText(alquiler.getNombre());
         precioAlquilerLabel.setText(String.valueOf(alquiler.getPrecio()+"€"));
 
@@ -44,7 +47,7 @@ public class MostrarAlquileres extends Conexion{
         ciudadAlquiler.setText(alquiler.getCiudad());
 
 
-        // Establecer la imagen en el ImageView
+        // Establecer la imagen del alquiler en el ImageView si está disponible
         if (alquiler.getImagen() != null) {
             imagenalquilerImageView.setImage(alquiler.getImagen());
         }
--- Proyecto/src/main/java/com/example/proyecto/MostrarLibros.java
+++ Proyecto/src/main/java/com/example/proyecto/MostrarLibros.java
@@ -4,8 +4,9 @@ import javafx.scene.layout.AnchorPane;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+// Clase MostrarLibros para mostrar la información de un libro específico
 public class MostrarLibros extends Conexion{
+    // Definición de los componentes de la interfaz gráfica que se enlazan con el archivo FXML
     @javafx.fxml.FXML
     private Label isbnLabel;
     @javafx.fxml.FXML
@@ -22,13 +23,14 @@ public class MostrarLibros extends Conexion{
     private AnchorPane panelMostrarLibros;
     @javafx.fxml.FXML
     private ImageView libroimagenImageView;
-    
+
+    // Objeto Libros que representa el libro que se va a mostrar
     Libros libros = new Libros();
-
+    // Método para establecer los datos del libro
     public void setData(Libros libros) {
-
+        // Asignar el objeto libro recibido al atributo de la clase
         this.libros = libros;
-
+        // Configurar las etiquetas con los datos del libro
         tituloLibroLabel.setText(libros.getTitulo());
         precioLabel.setText(libros.getPrecio() + "€");
         //Image image = new Image(getClass().getResourceAsStream(alquiler.getImgSrc()));
@@ -38,8 +40,7 @@ public class MostrarLibros extends Conexion{
         cursoLabel.setText("Curso: " + libros.getCurso() );
         isbnLabel.setText(libros.getIsbn());
 
-
-        // Establecer la imagen en el ImageView
+        // Establecer la imagen en el ImageView, si está disponible
         if (libros.getImagen() != null) {
             libroimagenImageView.setImage(libros.getImagen());
         }
